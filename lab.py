e280e6b5da68f2ce1e0050cb9a5f81f4be8c4ca0
Goose.exe <89397765+exeGOOSEexe@users.noreply.github.com>
2024-03-20 02:04:29 +0500

    Add files via upload

--- lab.py
+++ lab.py
@@ -1,15 +1,16 @@
-user_input1 = str(input("Enter a firth string: "))
-user_input2 = str(input("Enter a second string: "))
-def lab(user_input1, user_input2):
-    user_list = [i for i in user_input1]
-    user_list.sort()
-    user_list2 = [j for j in user_input2]
-    user_list2.sort()
-    for i in user_list:
-        if i not in user_list2:
-            return print(i)
-    for j in user_list2:
-        if j not in user_list:
-            return print(j)
-    return print("No missing letter")
-lab(user_input1, user_input2)
+user= ["user1", 15, ["user3"], 30, {"user5"}, ("user6"), {"user7": 1}, "user8", "user9", "user10"]
+
+def check(lst):
+    info = set()
+    for item in lst:
+        try:
+            info.add(item)
+        except TypeError:
+            continue
+        list_to_set(info)
+
+def list_to_set(lst):
+    print(lst)
+
+if __name__ == '__main__':
+    check(user)
