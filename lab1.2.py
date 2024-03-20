dd3e9a65c47035f1a72fff8524979449e4776894
Goose.exe <89397765+exeGOOSEexe@users.noreply.github.com>
2024-03-20 11:43:25 +0500

    Update lab1.2.py

--- lab1.2.py
+++ lab1.2.py
@@ -1,16 +1,19 @@
-user_input1 = str(input("Enter a firth string: "))
-user_input2 = str(input("Enter a second string: "))
+
 def lab(user_input1, user_input2):
     user_list = [i for i in user_input1]
-    user_list.sort()
     user_list2 = [j for j in user_input2]
-    user_list2.sort()
     for i in user_list:
-        if i not in user_list2:
-            return print(i)
-    for j in user_list2:
-        if j not in user_list:
-            return print(j)
-    return print("No missing letter")
-lab(user_input1, user_input2)
-// 2 лаба 2 задание
+        counter = 0
+        for j in user_list2:
+            if i == j:
+                counter += 1
+        if counter == 0:
+            return i
+    return ""
+
+
+user_input1 = input("Enter a firth string: ")
+user_input2 = input("Enter a second string: ")
+
+c = lab(user_input1, user_input2)
+print(c)
