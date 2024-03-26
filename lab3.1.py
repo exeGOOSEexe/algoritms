478da6536d8107aab4f6930deaca42bed49afeee
Goose.exe <89397765+exeGOOSEexe@users.noreply.github.com>
2024-03-26 21:08:04 +0500

    Update lab3.1.py

--- lab3.1.py
+++ lab3.1.py
@@ -10,4 +10,22 @@ def list_to_set(a):
     print(a)
 
 list_to_set(check)
-// 3 лаба 1 задание
+
+
+class SET:
+    def __init__(self, check_list):
+        self.check_list = check_list
+        self.list_to_set = set()
+    def add(self):
+        for i in self.check_list:
+            try:
+                self.list_to_set.add(i)
+            except TypeError:
+                continue
+def  print_set(a):
+    print(a)
+if __name__ == "__main__":
+    check_list = [1, "testd", 4.42, [5, 5,343], {23 : "test"}]
+    a = (SET(check_list))
+    a.add()
+    print_set(a.list_to_set)
