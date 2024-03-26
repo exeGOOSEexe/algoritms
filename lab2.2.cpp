099bf82f65025f7a769ea1c395f147aba46c44b6
Goose.exe <89397765+exeGOOSEexe@users.noreply.github.com>
2024-03-26 19:39:14 +0500

    Update lab2.2.cpp

--- lab2.2.cpp
+++ lab2.2.cpp
@@ -1,29 +1,33 @@
-#include <iostream>
-#include <stack>
+import random
+class Stack:
+    def __init__(self):
+        self.stack = []
+        self.min = None
 
-#define MAX 1000000
-void findmax(std::stack<int>& stack) {
-	int max = 0;
-	while (!stack.empty()) {
-		if (stack.top() > max) {
-			max = stack.top();
-		}
-		stack.pop();
-	}
-	std::cout << "Max number: " << max << std::endl;
-}
-int main() {
-	std::stack<int> stack;
-	int number = 0;
-	int n = 0;
+    def push(self, item):
+        self.stack.append(item)
+        if len(self.stack) == 1 or item < self.min:
+            self.min = item
 
-	std::cout << "Enter the number of random numbers to generate: ";
-	std::cin >> n;
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        removed = self.stack.pop()
+        if len(self.stack) == 0:
+            self.min = None
+        elif removed == self.min:
+            self.min = self.stack[0]
+            for value in self.stack:
+                if value < self.min:
+                    self.min =value
+        return removed
 
-	for (int i = 0; i < n; i++) {
-		number = rand() % MAX;
-		stack.push(number);
-	}
-	findmax(stack);
-	return 0;
-}
+        
+if __name__ == "__main__":
+    st = Stack()
+    print("Enter the number of elements to be pushed into the stack:")
+    user_input = int(input())
+    for i in range(user_input):
+        st.push(random.randint(10, 50))
+    print(st.stack)
+    print(st.min)
